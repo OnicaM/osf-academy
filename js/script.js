8cb8c1b86fc61c3c8d32dc46e3a56ba9b4a07f72
Oana <mac.oana92@yahoo.ro>
2019-11-08 03:41:20 +0200

    created functions for displaying dynamic data on landing page

--- js/script.js
+++ js/script.js
@@ -23,8 +23,69 @@ window.onload = function() {
       request.send();
     });
   });
+
+  function get(url) {
+    return new Promise(function(resolve, reject) {
+      var xhttp = new XMLHttpRequest();
+
+      xhttp.open("GET", url, true);
+      xhttp.onload = function() {
+        if (xhttp.status == 200) {
+          resolve(JSON.parse(xhttp.response));
+        } else {
+          reject(xhttp.statusText);
+        }
+      };
+      xhttp.onerror = function() {
+        reject(xhttp.statusText);
+      };
+      xhttp.send();
+    });
+  }
+
+  var promise = get("../json/popularItems.json");
+
+  promise.then(items => displayData(items.products));
 };
 
+function displayData(data) {
+  var container = document.querySelector(".products--popular .row");
+  var featuredContainer = document.querySelector(".products--featured .row");
+  data.forEach(item => {
+    var div = document.createElement("div");
+    var divF = document.createElement("div");
+    div.classList.add("col", "col-sm-12", "col-md-6", "col-lg-3", "item");
+    divF.classList.add("item");
+    div.innerHTML = `<div class="item_wrap">
+        <img src="/images/popularItems/${item.image}.png" alt="${item.name}">
+        <div class="item_caption">
+        <p>${item.name}</p>
+        <p class="price">$ ${item.price}</p>
+        </div>
+        </div>`;
+    divF.innerHTML = `<div class="item_wrap">
+        <img src="/images/popularItems/${item.image}.png" alt="${item.name}">
+        <div class="item_caption">
+        <p>${item.name}</p>
+        <p class="price">$ ${item.price}</p>
+        </div>
+        </div>`;
+    if (container) {
+      container.appendChild(div);
+    }
+    if (featuredContainer) {
+      featuredContainer.appendChild(divF);
+    }
+  });
+  $(".products--featured .row").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    adaptiveHeight: true
+  });
+}
 function toggleNav($trigger, $selector) {
   $trigger.on("click", function() {
     $selector.toggleClass("js--open");
@@ -40,8 +101,6 @@ $(".banner-slider").slick({
   arrows: false
 });
 
-function init() {
-  toggleClass($("li.has-children > a"), $(">ul"));
-}
+function init() {}
 
 init();
